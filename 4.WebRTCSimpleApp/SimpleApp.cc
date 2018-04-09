e63821852af627d059af53f7f81621d88fda0b17
David Bui <david.bui@cosmosoftware.io>
2018-04-09 13:08:40 +0800

    BUG: Porting to M66

--- 4.WebRTCSimpleApp/SimpleApp.cc
+++ 4.WebRTCSimpleApp/SimpleApp.cc
@@ -1,9 +1,12 @@
-#include "webrtc/api/peerconnectioninterface.h"
-
+#include "api/peerconnectioninterface.h"
+#include "api/audio_codecs/builtin_audio_decoder_factory.h"
+#include "api/audio_codecs/builtin_audio_encoder_factory.h"
 int main(int argc, char** argv) {
 
   rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface>
-  peer_connection_factory = webrtc::CreatePeerConnectionFactory();
+  peer_connection_factory = webrtc::CreatePeerConnectionFactory( 
+  		webrtc::CreateBuiltinAudioEncoderFactory(),
+    	webrtc::CreateBuiltinAudioDecoderFactory());
 
   return 0;
 }
